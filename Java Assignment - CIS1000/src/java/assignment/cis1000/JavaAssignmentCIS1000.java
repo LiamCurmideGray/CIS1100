9b4311d82b81f1f40bfaa5f4c3cb0f1793b69091
LiamCurmideGray <45282827+LiamCurmideGray@users.noreply.github.com>
2019-12-09 17:21:30 +0100

    Integer foo

--- Java Assignment - CIS1000/src/java/assignment/cis1000/JavaAssignmentCIS1000.java
+++ Java Assignment - CIS1000/src/java/assignment/cis1000/JavaAssignmentCIS1000.java
@@ -15,7 +15,8 @@ public class JavaAssignmentCIS1000 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int foo = 5;
+        System.out.println("This number is:" + foo);
     }
     
 }
